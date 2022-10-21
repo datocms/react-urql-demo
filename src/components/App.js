6d4b029a313058ad02fc6c5f96cb018f12c025de
Irene Oppo <i.oppo@datocms.com>
2022-10-21 16:31:51 +0200

    updated dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import Header from "./Header";
-import Home from "./Home";
-import About from "./About";
-import Recipe from "./Recipe";
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Recipe from './Recipe';
 
 const App = () => (
-  <Router>
+  <BrowserRouter>
     <Header />
     <main>
       <Route exact path="/" component={Home} />
       <Route path="/recipes/:slug" component={Recipe} />
       <Route path="/about" component={About} />
     </main>
-  </Router>
+  </BrowserRouter>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.sass";
-import App from "./components/App";
-import { Provider } from "urql";
-import client from "./client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.sass';
+import App from './components/App';
+import { Provider } from 'urql';
+import client from './client';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider value={client}>
     <App />
   </Provider>,
-  document.getElementById("root")
 );
